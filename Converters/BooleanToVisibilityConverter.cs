726180bd5bedbfae76a8956ce8b7763302762d30
mazx <mazx@ych.pl>
2025-05-15 18:16:09 +0200

    Zmodyfikowano: 2025-05-15 18:16:09

---
+++ Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,42 @@
+﻿// Plik: Converters/BooleanToVisibilityConverter.cs
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CosplayManager.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool boolValue = false;
+            if (value is bool b)
+            {
+                boolValue = b;
+            }
+
+            // Możliwość odwrócenia logiki przez parametr konwertera
+            if (parameter != null && parameter.ToString().Equals("Inverse", StringComparison.OrdinalIgnoreCase))
+            {
+                boolValue = !boolValue;
+            }
+
+            return boolValue ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility)
+            {
+                bool boolValue = visibility == Visibility.Visible;
+                if (parameter != null && parameter.ToString().Equals("Inverse", StringComparison.OrdinalIgnoreCase))
+                {
+                    boolValue = !boolValue;
+                }
+                return boolValue;
+            }
+            return false;
+        }
+    }
+}
