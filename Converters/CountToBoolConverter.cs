0197f2b5f0fac52788998b65b0034fe013660dd1
mazx <mazx@ych.pl>
2025-05-16 10:58:08 +0200

    Zmodyfikowano: 2025-05-16 10:58:07

---
+++ Converters/CountToBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿// Plik: Converters/CountToBoolConverter.cs
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CosplayManager.Converters
+{
+    public class CountToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int count)
+            {
+                return count > 0;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
