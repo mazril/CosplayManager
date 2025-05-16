7c8b9a9553da6747c655dcddc90911672ac8dc49
mazx <mazx@ych.pl>
2025-05-16 10:57:24 +0200

    Zmodyfikowano: 2025-05-16 10:57:24

---
+++ Converters/TypeCheckConverter.cs
@@ -0,0 +1,26 @@
+﻿// Plik: Converters/TypeCheckConverter.cs
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CosplayManager.Converters
+{
+    public class TypeCheckConverter : IValueConverter
+    {
+        public static TypeCheckConverter Instance { get; } = new TypeCheckConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter is not Type expectedType)
+            {
+                return false;
+            }
+            return expectedType.IsInstanceOfType(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
