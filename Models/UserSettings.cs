d2215c122d49964cbc92a3118c8dec68b5ed1811
mazx <mazx@ych.pl>
2025-05-16 14:29:26 +0200

    Zmodyfikowano: 2025-05-16 14:29:26

--- Models/UserSettings.cs
+++ Models/UserSettings.cs
@@ -1,19 +1,13 @@
 ﻿// Plik: Models/UserSettings.cs
-using System.Collections.Generic; // Jeśli SourceFolderNames będzie listą
-using System.ComponentModel; // Dla INotifyPropertyChanged, jeśli model ma być dynamiczny
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace CosplayManager.Models
 {
-    // Można dodać INotifyPropertyChanged, jeśli chcemy dynamicznie reagować na zmiany
-    // w samym obiekcie ustawień, ale zazwyczaj wystarczy odczyt/zapis przy starcie/zamknięciu.
-    public class UserSettings
+    public class UserSettings // Zmieniono na public
     {
         public string LibraryRootPath { get; set; } = string.Empty;
-        public string SourceFolderNamesInput { get; set; } = "Mix,Mieszane,Unsorted,Downloaded"; // Domyślne wartości
+        public string SourceFolderNamesInput { get; set; } = "Mix,Mieszane,Unsorted,Downloaded";
         public double SuggestionSimilarityThreshold { get; set; } = 0.85;
-
-        // Można dodać inne ustawienia w przyszłości, np.:
-        // public string LastUsedMixedFolderPath { get; set; } = string.Empty;
-        // public bool AutoLoadLastProfiles { get; set; } = true;
     }
 }
