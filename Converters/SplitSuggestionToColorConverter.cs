ede04239256e5d331f72a309e13d6e279833b212
mazx <mazx@ych.pl>
2025-05-16 10:58:33 +0200

    Zmodyfikowano: 2025-05-16 10:58:33

---
+++ Converters/SplitSuggestionToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿// Plik: Converters/SplitSuggestionToColorConverter.cs
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CosplayManager.Converters
+{
+    public class SplitSuggestionToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool hasSuggestion && hasSuggestion)
+            {
+                return Brushes.Purple; // Kolor dla sugestii podziału
+            }
+            return Brushes.Black; // Domyślny kolor
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
