5e6c4e16780b346555ea41533d51ace9f48b21c5
mazx <mazx@ych.pl>
2025-05-17 23:42:43 +0200

    Zmodyfikowano: 2025-05-17 23:42:43

--- Views/PreviewChangesWindow.xaml.cs
+++ Views/PreviewChangesWindow.xaml.cs
@@ -3,7 +3,7 @@ using CosplayManager.Services;
 using CosplayManager.ViewModels;
 using System;
 using System.Windows;
-using System.Windows.Controls; // Dla ItemsControl
+using System.Windows.Controls;
 using MahApps.Metro.Controls;
 
 namespace CosplayManager.Views
@@ -13,42 +13,40 @@ namespace CosplayManager.Views
         public PreviewChangesWindow()
         {
             InitializeComponent();
-            this.Closing += PreviewChangesWindow_Closing; // Dodajemy obsługę zdarzenia Closing
+            this.Closing += PreviewChangesWindow_Closing;
         }
 
-        // Metoda do ustawienia akcji zamykania z MainWindowViewModel
-        // Ta metoda powinna być wywoływana z MainWindowViewModel PO utworzeniu instancji PreviewChangesWindow
-        // a PRZED jej wyświetleniem (ShowDialog).
+        // Ta metoda jest wywoływana z MainWindowViewModel, aby przekazać akcję zamknięcia DO ViewModelu tego okna
         public void SetViewModelCloseAction(PreviewChangesViewModel vm)
         {
             if (vm != null)
             {
-                // Ustawiamy właściwość CloseAction w ViewModelu, aby ViewModel mógł wywołać zamknięcie okna
                 vm.CloseAction = (dialogResult) =>
                 {
-                    SimpleFileLogger.Log($"PreviewChangesWindow: CloseAction z ViewModelu wywołane z wynikiem: {dialogResult}.");
+                    SimpleFileLogger.Log($"PreviewChangesWindow: Akcja CloseAction z ViewModelu została wywołana z wynikiem: {dialogResult}.");
                     try
                     {
-                        // Ustawiamy DialogResult okna, co spowoduje jego zamknięcie
-                        // Należy to robić w wątku UI, jeśli ViewModel wywołuje to z innego wątku
-                        Application.Current.Dispatcher.Invoke(() =>
+                        // Ustaw DialogResult okna, co spowoduje jego zamknięcie
+                        // To powinno być bezpieczne do wywołania, nawet jeśli jest już zamykane.
+                        if (this.IsVisible) // Tylko jeśli okno jest nadal widoczne/aktywne jako dialog
                         {
-                            this.DialogResult = dialogResult;
-                            // Samo ustawienie DialogResult powinno zamknąć okno dialogowe.
-                            // Wywołanie this.Close() tutaj może być zbędne lub nawet powodować problemy, jeśli DialogResult już to robi.
-                        });
+                            Application.Current.Dispatcher.Invoke(() => this.DialogResult = dialogResult);
+                        }
                     }
                     catch (InvalidOperationException ioe)
                     {
-                        SimpleFileLogger.LogError($"PreviewChangesWindow: InvalidOperationException podczas ustawiania DialogResult: {ioe.Message}. Okno może nie zostać poprawnie zamknięte.", ioe);
-                        // Awaryjne zamknięcie, jeśli ustawienie DialogResult zawiedzie
-                        Application.Current.Dispatcher.Invoke(() => { if (this.IsVisible) this.Close(); });
+                        SimpleFileLogger.LogError($"PreviewChangesWindow: InvalidOperationException podczas ustawiania DialogResult: {ioe.Message}. Okno mogło nie zostać poprawnie zamknięte.", ioe);
+                        // Awaryjne zamknięcie, jeśli ustawienie DialogResult zawiedzie, a okno jest nadal widoczne
+                        Application.Current.Dispatcher.Invoke(() => { if (this.IsVisible && this.IsLoaded) this.Close(); });
                     }
                 };
             }
+            else
+            {
+                SimpleFileLogger.LogWarning("PreviewChangesWindow.SetViewModelCloseAction: Przekazany ViewModel był null.");
+            }
         }
 
-        // Metoda do czyszczenia zasobów przed faktycznym zamknięciem okna
         private void PrepareToCloseWindowResources()
         {
             SimpleFileLogger.Log("PreviewChangesWindow.PrepareToCloseWindowResources: Rozpoczęto czyszczenie zasobów okna.");
@@ -60,24 +58,17 @@ namespace CosplayManager.Views
 
             if (this.DataContext is PreviewChangesViewModel vm)
             {
-                // Jeśli ViewModel ma jakieś zasoby do zwolnienia, można to zrobić tutaj
-                // np. vm.Cleanup(); 
-                // Na razie ViewModel sam zarządza swoją logiką
+                // Dodatkowe czyszczenie w ViewModelu, jeśli jest potrzebne
+                // np. vm.CleanupBeforeClose();
             }
             this.DataContext = null;
             SimpleFileLogger.Log("PreviewChangesWindow.PrepareToCloseWindowResources: DataContext okna ustawiony na null.");
-
-            // Agresywne czyszczenie pamięci - używać ostrożnie i tylko jeśli są problemy z pamięcią
-            // GC.Collect();
-            // GC.WaitForPendingFinalizers();
-            // SimpleFileLogger.Log("PreviewChangesWindow.PrepareToCloseWindowResources: Wywołano GC.Collect().");
         }
 
-        // Obsługa zdarzenia Closing okna
         private void PreviewChangesWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
         {
-            SimpleFileLogger.Log("PreviewChangesWindow.Closing: Zdarzenie zamykania okna.");
-            PrepareToCloseWindowResources(); // Wyczyść zasoby
+            SimpleFileLogger.Log($"PreviewChangesWindow.Closing: Zdarzenie zamykania okna. DialogResult: {this.DialogResult}");
+            PrepareToCloseWindowResources();
         }
     }
 }
