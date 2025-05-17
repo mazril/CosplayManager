59fda551e9714b3655f25fdc417f1f3e6279fcf8
mazx <mazx@ych.pl>
2025-05-17 23:50:53 +0200

    Zmodyfikowano: 2025-05-17 23:50:53

--- ViewModels/ProposedMoveViewModel.cs
+++ ViewModels/ProposedMoveViewModel.cs
@@ -12,7 +12,7 @@ namespace CosplayManager.ViewModels
     public class ProposedMoveViewModel : ObservableObject
     {
         private readonly ProposedMove _move;
-        private bool _isApprovedForMove;
+        private bool _isApprovedForMove; // Nazwa właściwości używana w PreviewChangesViewModel
         private BitmapImage? _sourceThumbnail;
         private BitmapImage? _targetThumbnail;
         private bool _isLoadingSourceThumbnail;
@@ -21,14 +21,18 @@ namespace CosplayManager.ViewModels
         public ProposedMove OriginalMove => _move;
 
         public ImageFileEntry SourceImage => _move.SourceImage;
-        public ImageFileEntry? TargetImageDisplay => _move.TargetImageDisplay; // POPRAWIONA NAZWA
+        public ImageFileEntry? TargetImageDisplay => _move.TargetImage; // Używamy _move.TargetImage (zgodnie z Modelem)
         public string ProposedTargetPath => _move.ProposedTargetPath;
         public double Similarity => _move.Similarity;
         public string TargetCategoryProfileName => _move.TargetCategoryProfileName;
 
-        public ProposedMoveActionType Action { get => _move.Action; set { if (_move.Action != value) { _move.Action = value; OnPropertyChanged(); OnPropertyChanged(nameof(ActionDescription)); } } }
+        public ProposedMoveActionType Action
+        {
+            get => _move.Action;
+            set { if (_move.Action != value) { _move.Action = value; OnPropertyChanged(); OnPropertyChanged(nameof(ActionDescription)); } }
+        }
 
-        public bool IsApprovedForMove
+        public bool IsApprovedForMove // Ta nazwa jest używana w PreviewChangesViewModel
         {
             get => _isApprovedForMove;
             set => SetProperty(ref _isApprovedForMove, value);
@@ -61,7 +65,6 @@ namespace CosplayManager.ViewModels
         public string TargetFileNameDisplay => TargetImageDisplay != null ? Path.GetFileName(TargetImageDisplay.FilePath) :
                                              (Action == ProposedMoveActionType.CopyNew || Action == ProposedMoveActionType.OverwriteExisting ? Path.GetFileName(ProposedTargetPath) : "---");
 
-
         public string ActionDescription
         {
             get
@@ -81,7 +84,9 @@ namespace CosplayManager.ViewModels
         public ProposedMoveViewModel(Models.ProposedMove modelMove)
         {
             _move = modelMove;
+            // Domyślnie wszystko co nie jest NoAction jest do zatwierdzenia, chyba że logika PreviewChangesViewModel to zmieni
             _isApprovedForMove = (modelMove.Action != ProposedMoveActionType.NoAction);
+
             _ = LoadThumbnailsAsync();
         }
 
@@ -102,17 +107,17 @@ namespace CosplayManager.ViewModels
                 IsLoadingSourceThumbnail = false;
             }
 
-            if (TargetImageDisplay != null && TargetThumbnail == null && !IsLoadingTargetThumbnail) // POPRAWIONA NAZWA
+            if (TargetImageDisplay != null && TargetThumbnail == null && !IsLoadingTargetThumbnail)
             {
                 IsLoadingTargetThumbnail = true;
-                if (TargetImageDisplay.Thumbnail != null) // POPRAWIONA NAZWA
+                if (TargetImageDisplay.Thumbnail != null)
                 {
-                    TargetThumbnail = TargetImageDisplay.Thumbnail; // POPRAWIONA NAZWA
+                    TargetThumbnail = TargetImageDisplay.Thumbnail;
                 }
                 else
                 {
-                    await TargetImageDisplay.LoadThumbnailAsync(); // POPRAWIONA NAZWA
-                    TargetThumbnail = TargetImageDisplay.Thumbnail; // POPRAWIONA NAZWA
+                    await TargetImageDisplay.LoadThumbnailAsync();
+                    TargetThumbnail = TargetImageDisplay.Thumbnail;
                 }
                 IsLoadingTargetThumbnail = false;
             }
