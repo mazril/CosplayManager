c933f8afe7945515a82c8242f8a6ebf1ad6870d2
mazx <mazx@ych.pl>
2025-05-17 23:42:01 +0200

    Zmodyfikowano: 2025-05-17 23:42:01

--- Models/ProposedMove.cs
+++ Models/ProposedMove.cs
@@ -9,35 +9,34 @@ namespace CosplayManager.Models
         OverwriteExisting,
         KeepExistingDeleteSource,
         ConflictKeepBoth,
-        NoAction // Dodano dla jasności, gdy żadna akcja nie jest wymagana
+        NoAction
     }
 
     public class ProposedMove
     {
         public ImageFileEntry SourceImage { get; set; }
-        public ImageFileEntry? TargetImage { get; set; } // Pozostajemy przy TargetImage zgodnie z dostarczonym plikiem
+        public ImageFileEntry? TargetImageDisplay { get; set; } // Używamy tej nazwy
         public string ProposedTargetPath { get; set; }
         public double Similarity { get; set; }
         public string TargetCategoryProfileName { get; set; }
         public ProposedMoveActionType Action { get; set; }
 
         [JsonIgnore]
-        public float[]? SourceImageEmbedding { get; } // Dodana właściwość
+        public float[]? SourceImageEmbedding { get; }
 
-#pragma warning disable CS8618 
+#pragma warning disable CS8618
         public ProposedMove() { }
 #pragma warning restore CS8618
 
-        // Zaktualizowany konstruktor
-        public ProposedMove(ImageFileEntry sourceImage, ImageFileEntry? targetImage, string proposedTargetPath, double similarity, string targetCategoryProfileName, ProposedMoveActionType action, float[]? sourceEmbedding = null)
+        public ProposedMove(ImageFileEntry sourceImage, ImageFileEntry? targetImageDisplay, string proposedTargetPath, double similarity, string targetCategoryProfileName, ProposedMoveActionType action, float[]? sourceEmbedding = null)
         {
             SourceImage = sourceImage;
-            TargetImage = targetImage; // Używamy TargetImage
+            TargetImageDisplay = targetImageDisplay; // Używamy tej nazwy
             ProposedTargetPath = proposedTargetPath;
             Similarity = similarity;
             TargetCategoryProfileName = targetCategoryProfileName;
             Action = action;
-            SourceImageEmbedding = sourceEmbedding; // Przypisanie embeddingu
+            SourceImageEmbedding = sourceEmbedding;
         }
     }
 }
