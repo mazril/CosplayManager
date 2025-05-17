8ec2069d1678ba8c0029bfa4b547dd8a907251bd
mazx <mazx@ych.pl>
2025-05-17 23:40:15 +0200

    Zmodyfikowano: 2025-05-17 23:40:15

--- Models/ProposedMove.cs
+++ Models/ProposedMove.cs
@@ -1,5 +1,5 @@
 ﻿// Plik: Models/ProposedMove.cs
-using System.Text.Json.Serialization; // Dla JsonIgnore
+using System.Text.Json.Serialization;
 
 namespace CosplayManager.Models
 {
@@ -9,37 +9,30 @@ namespace CosplayManager.Models
         OverwriteExisting,
         KeepExistingDeleteSource,
         ConflictKeepBoth,
-        NoAction // Dodano, aby reprezentować brak akcji lub sytuację, gdy nic nie trzeba robić
+        NoAction // Dodano dla jasności, gdy żadna akcja nie jest wymagana
     }
 
     public class ProposedMove
     {
         public ImageFileEntry SourceImage { get; set; }
-        public ImageFileEntry? TargetImageDisplay { get; set; } // Zmieniono nazwę dla jasności, że to do wyświetlania
+        public ImageFileEntry? TargetImage { get; set; } // Pozostajemy przy TargetImage zgodnie z dostarczonym plikiem
         public string ProposedTargetPath { get; set; }
         public double Similarity { get; set; }
         public string TargetCategoryProfileName { get; set; }
         public ProposedMoveActionType Action { get; set; }
 
-        [JsonIgnore] // Embedding nie musi być serializowany, jeśli ProposedMove jest gdzieś zapisywany
-        public float[]? SourceImageEmbedding { get; } // Embedding obrazu źródłowego
+        [JsonIgnore]
+        public float[]? SourceImageEmbedding { get; } // Dodana właściwość
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        public ProposedMove() { } // Konstruktor bezparametrowy dla przypadków, gdy jest potrzebny (np. deserializacja, choć tu niezalecane)
+#pragma warning disable CS8618 
+        public ProposedMove() { }
 #pragma warning restore CS8618
 
-        // Główny konstruktor
-        public ProposedMove(
-            ImageFileEntry sourceImage,
-            ImageFileEntry? targetImageDisplay, // Zmieniono nazwę parametru
-            string proposedTargetPath,
-            double similarity,
-            string targetCategoryProfileName,
-            ProposedMoveActionType action,
-            float[]? sourceEmbedding = null) // Dodano opcjonalny parametr embeddingu
+        // Zaktualizowany konstruktor
+        public ProposedMove(ImageFileEntry sourceImage, ImageFileEntry? targetImage, string proposedTargetPath, double similarity, string targetCategoryProfileName, ProposedMoveActionType action, float[]? sourceEmbedding = null)
         {
             SourceImage = sourceImage;
-            TargetImageDisplay = targetImageDisplay;
+            TargetImage = targetImage; // Używamy TargetImage
             ProposedTargetPath = proposedTargetPath;
             Similarity = similarity;
             TargetCategoryProfileName = targetCategoryProfileName;
