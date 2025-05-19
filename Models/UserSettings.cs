76ad941ab455300e73d68311bbe644e9efc33ec3
mazx <mazx@ych.pl>
2025-05-19 13:05:22 +0200

    Zmodyfikowano: 2025-05-19 13:05:22

--- Models/UserSettings.cs
+++ Models/UserSettings.cs
@@ -4,10 +4,11 @@ using System.ComponentModel;
 
 namespace CosplayManager.Models
 {
-    public class UserSettings // Zmieniono na public
+    public class UserSettings
     {
         public string LibraryRootPath { get; set; } = string.Empty;
         public string SourceFolderNamesInput { get; set; } = "Mix,Mieszane,Unsorted,Downloaded";
         public double SuggestionSimilarityThreshold { get; set; } = 0.85;
+        public bool EnableDebugLogging { get; set; } = false; // <-- DODANA WŁAŚCIWOŚĆ
     }
 }
